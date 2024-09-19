0c36ccd35f7a57c6ca5f3c566205884d26b7c7d5
rohan <rohannair2939@gmail.com>
2024-09-19 22:18:10 +0530

    color assignement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video,save_video
 from trackers import Tracker
+import cv2
 
 
 def main():
@@ -7,6 +8,15 @@ def main():
     
     tracker=Tracker('models/best.pt') #initialize tracker
     tracks=tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path='stubs/track_stubs.pkl')
+    
+    # #save cropped image of a player
+    # for track_id,player in tracks['players'][0].items():
+    #     bbox=player['bbox']
+    #     frame=video_frames[0]
+    #     cropped_image=frame[int(bbox[1]):int(bbox[3]),int(bbox[0]):int(bbox[2])]
+    #     cv2.imwrite(f'output_videos/cropped_img.jpg',cropped_image)
+    #     break
+        
     
     #draw output
     output_video_frames = tracker.draw_annotations(video_frames,tracks)
