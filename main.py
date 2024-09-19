d635abc40c1bb50534863b6aef71168486a73b91
rohan <rohannair2939@gmail.com>
2024-09-19 09:05:30 +0530

    annotations

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def main():
     tracker=Tracker('models/best.pt') #initialize tracker
     tracks=tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path='stubs/track_stubs.pkl')
     
-    save_video(video_frames,'output_videos/output_video.avi') #saving
+    #draw output
+    output_video_frames = tracker.draw_annotations(video_frames,tracks)
+    
+    save_video(output_video_frames,'output_videos/output_video.avi') #saving
     
 if __name__ == '__main__':
     main()
